9946b9e8c4a2454de8c1cde0e47ed8309cb31fef
Alex Meade <mr.alex.meade@gmail.com>
2019-06-17 01:58:05 +0000

    Fix using localhost in javascript

--- src/main/resources/static/js/flights.js
+++ src/main/resources/static/js/flights.js
@@ -1,4 +1,4 @@
-var evtSource = new EventSource("http://localhost:8080/flights");
+var evtSource = new EventSource("http://" + window.location.hostname + ":8080/flights");
 var map = new google.maps.Map(document.getElementById('map_canvas'), {
     zoom: 8,
     center: new google.maps.LatLng(37.77, -122.43),
@@ -112,7 +112,7 @@ function plane_info(state) {
 // update Aircraft info card
 function aircraft(icao) {
     $.get(
-        "http://localhost:8080/aircraft/"+ icao ,
+        "http://" + window.location.hostname + ":8080/aircraft/"+ icao ,
         function(data) {
                 content = '<div class="card-content white-text">' +
                     '<span class="card-title">Aircraft Info: ICAO 24 ' + data.icao + '</span> ' +
